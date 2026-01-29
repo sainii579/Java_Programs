5728840fb5ab4949262915720cc3c96488e78ffd
Varsha Tavdival <varshatavdival@gmail.com>
2026-01-29 14:04:26 +0530

    Add files via upload

---
+++ ThreadUserDefinedSynchronised.java
@@ -0,0 +1,16 @@
+class Demo extends Thread
+{
+    public void run() 
+    {
+        
+    }
+}
+
+class ThreadUserDefinedSynchronized
+{
+    public static void main(String A[]) 
+    {
+        
+            
+    }
+}
